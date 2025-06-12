d5e1de01dc71aeb2d9d65b73f96f7914eaff253f
Leonardo Merza <ljmerza@gmail.com>
2025-06-11 22:27:36 -0400

    t

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { getEntityIds, hasConfigOrEntitiesChanged, hasGenericSecondaryInfo, hide
 import { style } from './styles';
 
 console.info(
-    '%c MULTIPLE-ENTITY-ROW-CHORES %c x.x.1 ',
+    '%c MULTIPLE-ENTITY-ROW-CHORES %c 10.1.1 ',
     'color: cyan; background: black; font-weight: bold;',
     'color: darkblue; background: white; font-weight: bold;'
 );
